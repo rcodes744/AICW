639a9d023c01cef2e36d553d508bad455a043a9b
unknown <riyathakkar744@gmail.com>
2026-01-08 12:16:58 +0530

    modified commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+print('hello')
+import numpy as np
+print(np.__version__)
+import sklearn
+print(sklearn.__version__)
+import tensorflow as tf
+print(tf.__version__)
+# environment  create and activate      (environment)python -m venv en
+#en/Scripts/activate
