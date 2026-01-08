a5dca7f1220f51f802329d05182e91312d13c746
unknown <riyathakkar744@gmail.com>
2026-01-08 12:52:33 +0530

    modify commit with new file

--- demo.py
+++ demo.py
@@ -7,3 +7,12 @@ import tensorflow as tf
 print(tf.__version__)
 # environment  create and activate      (environment)python -m venv en
 #en/Scripts/activate
+#git init
+
+# in C:\Users\Riya Thakkar\Desktop\ms  cer/.git/
+#git add demo.py
+#git commit -m 'first commit'
+#git status
+#git config --global user.email "riyathakkar744@gmail.com"
+#git commit -m 'modified commit'
+#git add demo.py next.py
