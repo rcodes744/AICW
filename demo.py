72af69a6c913968a82d70b5b3d5bf690f97c0f01
unknown <riyathakkar744@gmail.com>
2026-01-10 15:55:04 +0530

    Fixed Streamlit FitTRACK app indentation and added features

--- demo.py
+++ demo.py
@@ -5,6 +5,16 @@ import sklearn
 print(sklearn.__version__)
 import tensorflow as tf
 print(tf.__version__)
+
+# in power shell Set-ExecutionPolicy Unrestricted
+#A
+#Get-ExectionPolicy
+
+#pip install numpy
+#pip install sklearn
+#pip install tensorflow
+#python demo.py
+
 # environment  create and activate      (environment)python -m venv en
 #en/Scripts/activate
 #git init
@@ -16,3 +26,8 @@ print(tf.__version__)
 #git config --global user.email "riyathakkar744@gmail.com"
 #git commit -m 'modified commit'
 #git add demo.py next.py
+
+
+# python file is save in memeory hardware 
+# run in ram ,where we are doing all the computation and calculation it is going ram to processor     
+#CPU processes calculations in RAM during execution
